5a78b9c961f347b63b0e4a6c79a60b26eb03bb27
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-10-07 22:40:19 +0800

    creating_angular_client-app

---
+++ client-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExpenseComponent } from './expense/expense.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExpenseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
